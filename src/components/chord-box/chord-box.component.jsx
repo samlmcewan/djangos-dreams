99ada1479f9625b545f0ebd4488ebf28bdd67358
samlmcewan <samlmcewan@gmail.com>
2020-11-08 18:44:33 +0000

    Create split boxes for boxes with 2 chords. Add more data to data.js

--- src/components/chord-box/chord-box.component.jsx
+++ src/components/chord-box/chord-box.component.jsx
@@ -2,12 +2,36 @@ import React from 'react';
 import './chord-box.styles.scss';
 
 const ChordBox = ({note}) => {
+    // add conditional statement here saying if note is whole box do below, if not make another return statement for trianlge/half boxes
+    if (typeof note === 'object') { 
+
+        return (
+            // <div className="chord-box">
+            //     <div className="chord-box-half-one">
+            //         <p className="chord-note">{note.chordOne}</p>
+            //     </div>
+            //     <div className="chord-box-half-two">
+            //         <p className="chord-note">{note.chordTwo}</p>
+            //     </div>
+            // </div>
+            <div className="chord-box-split">
+                
+                    <p className="chord-note half-left">{note.chordOne}</p>
+                    <div className="half-line"></div>
+                    <p className="chord-note half-right">{note.chordTwo}</p>
+                
+            </div>
+        );
+
+}
+else {
+    
     return (
         <div className="chord-box">
             <p className="chord-note">{note}</p>
         </div>
     );
 
-}
+} };
 
 export default ChordBox;
--- src/mock-data/data.js
+++ src/mock-data/data.js
@@ -2,38 +2,34 @@ const SongData = [
     {   
         title: "After You've Gone",
         year: 1936,
-        chords: ['C', '%', 'Cm', '%', 'G', '%', 'E7', '%', 'A7', '%', 'D7', '%', 'G', '%', 'G7', '%', 'C', '%', 'Cm', '%', 'G', '%', 'E7', '%', 'Am', 'E7', 'Am', 'Cm', 'G', 'B7', 'Em', 'Edim', 'G', '%', 'Am7', 'D7', 'G', '%', '%', 'G7'],
+        chords: ['C', '%', 'Cm', '%', 'G', '%', 'E7', '%', 'A7', '%', 'D7', '%', 'G', '%', 'G7', '%', 'C', '%', 'Cm', '%', 'G', '%', 'E7', '%', 'Am', 'E7', 'Am', 'Cm', 'G', 'B7', 'Em', 'Edim', 'G', '%', 'Am7', 'D7', 'G', '%', '%', 'G7']
     },
     {   
-        title: "Another Song",
+        title: "Nagasaki",
         year: 1936,
-        chords: ['C', '%', 'Cm', '%', 'G', '%', 'E7', '%', 'A7', '%', 'D7', '%', 'G', '%', 'G7', '%', 'C', '%', 'Cm', '%', 'G', '%', 'E7', '%', 'Am', 'E7', 'Am', 'Cm', 'G', 'B7', 'Em', 'Edim', 'G', '%', 'Am7', 'D7', 'G', '%', '%', 'G7'],
+        chords: [{chordOne: 'C', chordTwo: 'C#ø'}, 'G7/D', {chordOne: 'C', chordTwo: 'C#ø'}, 'G7/B', {chordOne: 'C', chordTwo: 'E7'}, {chordOne:'F', chordTwo:'Fm'}, {chordOne: 'C', chordTwo:'G7'}, 'C', {chordOne: 'C', chordTwo: 'C#ø'}, 'G7/D', {chordOne: 'C', chordTwo: 'C#ø'}, 'G7/B', {chordOne: 'C', chordTwo: 'E7'}, {chordOne:'F', chordTwo:'Fm'}, {chordOne: 'C', chordTwo:'G7'}, 'C', 'F', 'Fm', 'C', 'C7', 'F', 'Fm', {chordOne: 'C', chordTwo: 'Am'}, {chordOne: 'D7', chordTwo:'G7'}, {chordOne: 'C', chordTwo: 'C#ø'}, 'G7/D', {chordOne: 'C', chordTwo: 'C#ø'}, 'G7/B', {chordOne: 'C', chordTwo: 'E7'}, {chordOne:'F', chordTwo:'Fm'}, {chordOne: 'C', chordTwo:'G7'}, 'C']
     },
     {   
-        title: "After You've Gone",
+        title: "Coucou",
         year: 1936,
-        chords: ['C', '%', 'Cm', '%', 'G', '%', 'E7', '%', 'A7', '%', 'D7', '%', 'G', '%', 'G7', '%', 'C', '%', 'Cm', '%', 'G', '%', 'E7', '%', 'Am', 'E7', 'Am', 'Cm', 'G', 'B7', 'Em', 'Edim', 'G', '%', 'Am7', 'D7', 'G', '%', '%', 'G7'],
+        chords: ['C', '%', 'Am', '%', 'F', 'G7', {chordOne: 'C', chordTwo: 'Am7'}, {chordOne: 'Dm7', chordTwo: 'G7'}, 'C', '%', 'Am', '%', 'F', 'G7', 'C', '%', 'C7', '%', 'F', '%', 'D7', '%', 'G7', '%', 'C', '%', 'Am', '%', 'F', 'G7', 'C', '(G7)']
     },
     {   
-        title: "After You've Gone",
+        title: "Melodie Au Crepuscule (Twilights Melody)",
         year: 1936,
-        chords: ['C', '%', 'Cm', '%', 'G', '%', 'E7', '%', 'A7', '%', 'D7', '%', 'G', '%', 'G7', '%', 'C', '%', 'Cm', '%', 'G', '%', 'E7', '%', 'Am', 'E7', 'Am', 'Cm', 'G', 'B7', 'Em', 'Edim', 'G', '%', 'Am7', 'D7', 'G', '%', '%', 'G7'],
+        chords: [{chordOne: 'Em', chordTwo: 'Em7'}, 'A7', 'D', '%', 'E9', '%', 'E♭9', 'A7', {chordOne: 'Em', chordTwo: 'Em7'}, {chordOne: 'A/C#', chordTwo: 'A7#5'}, 'D', {chordOne: 'D', chordTwo: 'Bm'}, 'Em', {chordOne: 'E♭9', chordTwo: 'A7#5'}, 'D 6/9', '-', {chordOne: 'Em', chordTwo: 'Em7'}, {chordOne: 'A/C#', chordTwo: 'A7#5'}, {chordOne: 'D', chordTwo: 'Gm6'}, {chordOne: 'D', chordTwo: 'C#9'}, {chordOne: 'F#', chordTwo: 'D#m'}, {chordOne: 'G#m7', chordTwo: 'C#9'}, {chordOne: 'F#6', chordTwo: 'C#9'}, {chordOne: 'F#', chordTwo: 'F#ø'}, {chordOne: 'Em', chordTwo: 'C#7'}, {chordOne: 'C7/G', chordTwo: 'F#ø'}, 'Em', {chordOne: 'F#7', chordTwo: 'F7'}, 'Bm', 'Gm6', 'A7', '-', 'Em', 'A7', 'F#m♭5', 'F#ø', 'Em', {chordOne: 'E7', chordTwo: 'A7'}, {chordOne: 'D', chordTwo: 'A7'}, 'D']
     },
     {   
-        title: "After You've Gone",
+        title: "Georgia On My Mind",
         year: 1936,
-        chords: ['C', '%', 'Cm', '%', 'G', '%', 'E7', '%', 'A7', '%', 'D7', '%', 'G', '%', 'G7', '%', 'C', '%', 'Cm', '%', 'G', '%', 'E7', '%', 'Am', 'E7', 'Am', 'Cm', 'G', 'B7', 'Em', 'Edim', 'G', '%', 'Am7', 'D7', 'G', '%', '%', 'G7'],
+        chords: ['E♭', 'G7', 'Cm', {chordOne: 'A♭', chordTwo: 'A♭m'}, {chordOne: 'E♭', chordTwo: 'F#ø'}, {chordOne: 'Fm', chordTwo: 'B♭7'}, {chordOne: 'E♭', chordTwo: 'Eø'}, {chordOne: 'Fm', chordTwo: 'B♭7'}, 'E♭', 'G7', 'Cm', {chordOne: 'A♭', chordTwo: 'A♭m'}, {chordOne: 'E♭', chordTwo: 'F#ø'}, {chordOne: 'Fm', chordTwo: 'B♭7'}, {chordOne: 'E♭', chordTwo: 'A♭m'}, {chordOne: 'E♭', chordTwo: 'G7'}, 'Cm', {chordOne: '%', chordTwo: 'G7'}, 'Cm', {chordOne: '%', chordTwo: 'F'}, 'Cm', {chordOne: '%', chordTwo: 'D7'}, {chordOne: 'Gm', chordTwo: 'G♭ø'}, {chordOne: 'Fm', chordTwo: 'B♭7'}, 'E♭', 'G7', 'Cm', {chordOne: 'A♭', chordTwo: 'A♭m'}, {chordOne: 'E♭', chordTwo: 'F#ø'}, {chordOne: 'Fm', chordTwo: 'B♭7'}, {chordOne: 'E♭', chordTwo: 'A♭m'}, 'E♭']
     },
     {   
-        title: "After You've Gone",
+        title: "I Can't Give You Anything But Love",
         year: 1936,
-        chords: ['C', '%', 'Cm', '%', 'G', '%', 'E7', '%', 'A7', '%', 'D7', '%', 'G', '%', 'G7', '%', 'C', '%', 'Cm', '%', 'G', '%', 'E7', '%', 'Am', 'E7', 'Am', 'Cm', 'G', 'B7', 'Em', 'Edim', 'G', '%', 'Am7', 'D7', 'G', '%', '%', 'G7'],
+        chords: ['G', {chordOne: 'G/B', chordTwo: 'B♭ø'}, 'Am7', 'D7', 'G', {chordOne: 'G/B', chordTwo: 'B♭ø'}, 'Am7', 'D7', 'G7', '%', 'C', '%', 'A7', '%', 'D7', '%', 'G', {chordOne: 'G/B', chordTwo: 'B♭ø'}, 'Am7', 'D7', 'G7', '%', 'C', '%', 'C', 'C#ø', 'G/D', 'E7', 'Am7', 'D7', {chordOne: 'G', chordTwo: 'E7'}, {chordOne: 'Am7', chordTwo: 'D7'}]
     },
-    {   
-        title: "After You've Gone",
-        year: 1936,
-        chords: ['C', '%', 'Cm', '%', 'G', '%', 'E7', '%', 'A7', '%', 'D7', '%', 'G', '%', 'G7', '%', 'C', '%', 'Cm', '%', 'G', '%', 'E7', '%', 'Am', 'E7', 'Am', 'Cm', 'G', 'B7', 'Em', 'Edim', 'G', '%', 'Am7', 'D7', 'G', '%', '%', 'G7'],
-    }
+   
 
 ];
 
