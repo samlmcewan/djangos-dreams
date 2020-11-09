d1c85ef86e1c70161c319f6080ed8e65492c3f72
samlmcewan <samlmcewan@gmail.com>
2020-11-09 00:36:07 +0000

    Added fret finger position data to chord diagram component

--- src/components/chord-diagram/chord-diagram.component.jsx
+++ src/components/chord-diagram/chord-diagram.component.jsx
@@ -10,6 +10,48 @@ const ChordDiagram = (positions) => {
 
     const renderedFrets = frets.map(fret => <div className="fret" id={fret}></div>);
 
+    const fingerDots = [
+        {fret: '1', guitarString: 'e6'},
+        {fret: '1', guitarString: 'a5'},
+        {fret: '1', guitarString: 'd4'},
+        {fret: '1', guitarString: 'g3'},
+        {fret: '1', guitarString: 'b2'},
+        {fret: '1', guitarString: 'e1'},
+
+        {fret: '2', guitarString: 'e6'},
+        {fret: '2', guitarString: 'a5'},
+        {fret: '2', guitarString: 'd4'},
+        {fret: '2', guitarString: 'g3'},
+        {fret: '2', guitarString: 'b2'},
+        {fret: '2', guitarString: 'e1'},
+
+        {fret: '3', guitarString: 'e6'},
+        {fret: '3', guitarString: 'a5'},
+        {fret: '3', guitarString: 'd4'},
+        {fret: '3', guitarString: 'g3'},
+        {fret: '3', guitarString: 'b2'},
+        {fret: '3', guitarString: 'e1'},
+
+        {fret: '4', guitarString: 'e6'},
+        {fret: '4', guitarString: 'a5'},
+        {fret: '4', guitarString: 'd4'},
+        {fret: '4', guitarString: 'g3'},
+        {fret: '4', guitarString: 'b2'},
+        {fret: '4', guitarString: 'e1'},
+
+        {fret: '5', guitarString: 'e6'},
+        {fret: '5', guitarString: 'a5'},
+        {fret: '5', guitarString: 'd4'},
+        {fret: '5', guitarString: 'g3'},
+        {fret: '5', guitarString: 'b2'},
+        {fret: '5', guitarString: 'e1'}
+
+    ];
+
+    const renderedFingerDots = fingerDots.map(dot => <div className="dot" id={`${dot.fret}-${dot.guitarString}`}></div>);
+
+    console.log(renderedFingerDots);
+
 
     return (
         <div className="chord-diagram-container">
