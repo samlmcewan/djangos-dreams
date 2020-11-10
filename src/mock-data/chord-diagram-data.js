975cc1ac76883cc00b01392f622498bc24301fd5
samlmcewan <samlmcewan@gmail.com>
2020-11-10 17:14:06 +0000

    Move guitar data out of chord diagram component into new file chord diagram data.js.Star to create getXos function to conditionally render x or o on chord diagram component

---
+++ src/mock-data/chord-diagram-data.js
@@ -0,0 +1,43 @@
+export const guitarStrings = ['e6', 'a5', 'd4', 'g3', 'b2', 'e1'];
+
+export const frets = ['fret-1', 'fret-2', 'fret-3', 'fret-4'];
+
+export const stringXOs = [{str: 'e6', xo: 'x'}, {str: 'a5', xo: 'x'}, {str: 'd4', xo: 'x'}, {str: 'g3', xo: 'x'}, {str: 'b2', xo: 'x'}, {str: 'e1', xo: 'x'}];
+
+export const fingerDots = [
+    {guitarString: 'e6', fret: '1', strFret: 'e61', isVisible: 'hidden'},
+    {guitarString: 'e6', fret: '2', strFret: 'e62', isVisible: 'hidden'},
+    {guitarString: 'e6', fret: '3', strFret: 'e63', isVisible: 'hidden'},
+    {guitarString: 'e6', fret: '4', strFret: 'e64', isVisible: 'hidden'},
+    {guitarString: 'e6', fret: '5', strFret: 'e65', isVisible: 'hidden'},
+
+    {guitarString: 'a5', fret: '1', strFret: 'a51', isVisible: 'hidden'},
+    {guitarString: 'a5', fret: '2', strFret: 'a52', isVisible: 'hidden'},
+    {guitarString: 'a5', fret: '3', strFret: 'a53', isVisible: 'hidden'},
+    {guitarString: 'a5', fret: '4', strFret: 'a54', isVisible: 'hidden'},
+    {guitarString: 'a5', fret: '5', strFret: 'a55', isVisible: 'hidden'},
+
+    {guitarString: 'd4', fret: '1', strFret: 'd41', isVisible: 'hidden'},
+    {guitarString: 'd4', fret: '2', strFret: 'd42', isVisible: 'hidden'},
+    {guitarString: 'd4', fret: '3', strFret: 'd43', isVisible: 'hidden'},
+    {guitarString: 'd4', fret: '4', strFret: 'd44', isVisible: 'hidden'},
+    {guitarString: 'd4', fret: '5', strFret: 'd45', isVisible: 'hidden'},
+
+    {guitarString: 'g3', fret: '1', strFret: 'g31', isVisible: 'hidden'},
+    {guitarString: 'g3', fret: '2', strFret: 'g32', isVisible: 'hidden'},
+    {guitarString: 'g3', fret: '3', strFret: 'g33', isVisible: 'hidden'},
+    {guitarString: 'g3', fret: '4', strFret: 'g34', isVisible: 'hidden'},
+    {guitarString: 'g3', fret: '5', strFret: 'g35', isVisible: 'hidden'},
+
+    {guitarString: 'b2', fret: '1', strFret: 'b21', isVisible: 'hidden'},
+    {guitarString: 'b2', fret: '2', strFret: 'b22', isVisible: 'hidden'},
+    {guitarString: 'b2', fret: '3', strFret: 'b23', isVisible: 'hidden'},
+    {guitarString: 'b2', fret: '4', strFret: 'b24', isVisible: 'hidden'},
+    {guitarString: 'b2', fret: '5', strFret: 'b25', isVisible: 'hidden'},
+
+    {guitarString: 'e1', fret: '1', strFret: 'e11', isVisible: 'hidden'},
+    {guitarString: 'e1', fret: '2', strFret: 'e12', isVisible: 'hidden'},
+    {guitarString: 'e1', fret: '3', strFret: 'e13', isVisible: 'hidden'},
+    {guitarString: 'e1', fret: '4', strFret: 'e14', isVisible: 'hidden'},
+    {guitarString: 'e1', fret: '5', strFret: 'e15', isVisible: 'hidden'},
+];
