b62f1664b5816706279227a614e1e1e37a47501d
samlmcewan <samlmcewan@gmail.com>
2020-11-17 00:12:17 +0000

    Add logo and CSS rotating/swing animation to it

--- src/App.js
+++ src/App.js
@@ -2,16 +2,28 @@ import ChordChartContainer from './components/chord-chart-container/chord-chart-
 
 import SongData from './app-data/data';
 
+import { Frame } from "framer";
+
+import logo from './DjangosDreamsLogo.png'
+
 import './App.css';
 
 const songs = SongData;
+
+const transition = {
+  loop: Infinity,
+  ease: "linear",
+  duration: 2
+}
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <div className="swing"><img src={logo} className="App-logo" alt="logo" /></div>
       <h1>Djangos Dreams</h1>
-      <p>Chord charts of classic Django Reinhardt numbers</p>
+      <p id="header-tagline">Django Reinhardt chord charts with diagrams</p>
       </header>
       <main className="App-main">
       <div className='container-main'>
